618cde4a8cd9e3c0598b34544a7ecfa515456608
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2014-06-15 07:37:00 +1000

    Add bespoke-scale

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,15 @@
 var bespoke = require('bespoke'),
   keys = require('bespoke-keys'),
   touch = require('bespoke-touch'),
-  bullets = require('bespoke-bullets');
+  bullets = require('bespoke-bullets'),
+  scale = require('bespoke-scale');
 
 // Bespoke.js
 bespoke.from('article', [
   keys(),
   touch(),
-  bullets('li, .bullet')
+  bullets('li, .bullet'),
+  scale()
 ]);
 
 // Prism syntax highlighting
