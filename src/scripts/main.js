93444328eb146ebbe140a2bd89a0ed122b50ab6f
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2014-06-15 10:27:41 +1000

    Add bespoke-hash

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,16 @@ var bespoke = require('bespoke'),
   keys = require('bespoke-keys'),
   touch = require('bespoke-touch'),
   bullets = require('bespoke-bullets'),
-  scale = require('bespoke-scale');
+  scale = require('bespoke-scale'),
+  hash = require('bespoke-hash');
 
 // Bespoke.js
 bespoke.from('article', [
   keys(),
   touch(),
   bullets('li, .bullet'),
-  scale()
+  scale(),
+  hash()
 ]);
 
 // Prism syntax highlighting
