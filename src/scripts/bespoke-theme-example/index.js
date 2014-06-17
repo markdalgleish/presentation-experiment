67fba0fb0086675a1f57702345c8fdc44142c839
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2014-06-17 12:06:44 +1000

    Use insert-css with prepend option \o/

--- src/scripts/bespoke-theme-example/index.js
+++ src/scripts/bespoke-theme-example/index.js
@@ -1,32 +1,11 @@
-// TODO: Make this a standalone module?
-var prependCss = (function() {
-  var inserted = {};
-
-  return function(css) {
-    if (inserted[css]) return;
-    inserted[css] = true;
-
-    var elem = document.createElement('style');
-    elem.setAttribute('type', 'text/css');
-
-    if ('textContent' in elem) {
-      elem.textContent = css;
-    } else {
-      elem.styleSheet.cssText = css;
-    }
-
-    var head = document.getElementsByTagName('head')[0];
-    head.insertBefore(elem, head.children[0]);
-  };
-}());
-
 var fs = require('fs');
 var classes = require('../bespoke-classes');
+var insertCss = require('insert-css');
 
 module.exports = function() {
   return function(deck) {
     classes()(deck);
     var css = fs.readFileSync(__dirname + '/theme.css', 'utf8');
-    prependCss(css);
+    insertCss(css, { prepend: true });
   };
 };
